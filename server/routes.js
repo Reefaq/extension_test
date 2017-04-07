7a2fe2c558cef5b5834939cd99a84e8fdd5768c6
Priyanka Mistry <priyankamistry09@users.noreply.github.com>
2017-04-07 12:52:13 +0530

    Update routes.js
    version number changed for swapnil api

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
 var when = require('when')
 
 module.exports = {
-	"/v1/functions/swapnil": {
+	"/v2/functions/swapnil": {
 		GET: function(req, res){
 			this.resSuccess(req, res, {
 				swapnil: "is awesome"
